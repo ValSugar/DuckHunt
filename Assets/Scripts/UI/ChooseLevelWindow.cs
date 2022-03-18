4b0c8ebf37cabda0b9f84c3e525ed8c66d4c31ec
ValiantSugar <ValiantSugar@gmail.com>
2022-03-18 16:11:54 +0300

    Update chooseLevelWindow

--- Assets/Scripts/UI/ChooseLevelWindow.cs
+++ Assets/Scripts/UI/ChooseLevelWindow.cs
@@ -1,5 +1,6 @@
 using EventLayer;
 using Level;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +8,7 @@ namespace UI
 {
     public class ChooseLevelWindow : MonoBehaviour
     {
+		[SerializeField] private TMP_Text _levelIndexLabel;
 		[SerializeField] private Transform _content;
 		[SerializeField] private Button _backButton;
         [SerializeField] private TextButton _levelButtonPrefab;
@@ -22,10 +24,13 @@ namespace UI
 			}
 
 			_backButton.onClick.AddListener(() => gameObject.SetActive(false));
+
+			_levelIndexLabel.SetText($"Choosen Level: {LevelBus.ChoosenLevel.Value}");
 		}
 
 		private void OnLevelButtonClick(int levelNumber)
 		{
+			_levelIndexLabel.SetText($"Choosen Level: {levelNumber}");
 			LevelBus.ChoosenLevel.Publish(levelNumber);
 		}
 	}
